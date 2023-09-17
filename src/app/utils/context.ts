1db509c1999583e624f381c12a2ee4e8043204f1
Rami Maalouf <rami.rami@ucalgary.ca>
2023-09-17 04:53:46 -0400

    added more info on project

--- src/app/utils/context.ts
+++ src/app/utils/context.ts
@@ -13,19 +13,23 @@ export const getContext = async (message: string, namespace: string, maxTokens =
 
   // Get the embeddings of the input message
   const embedding = await getEmbeddings(message);
+  console.log("embedding", embedding.length);
 
   // Retrieve the matches for the embeddings from the specified namespace
   const matches = await getMatchesFromEmbeddings(embedding, 3, namespace);
+  console.log("matches", matches.length);
 
   // Filter out the matches that have a score lower than the minimum score
-  const qualifyingDocs = matches.filter(m => m.score && m.score > minScore);
+  const qualifyingDocs = matches.filter((m) => m.score && m.score > minScore);
+  console.log("qualifyingDocs", qualifyingDocs.length);
 
   if (!getOnlyText) {
     // Use a map to deduplicate matches by URL
-    return qualifyingDocs
+    return qualifyingDocs;
   }
 
-  let docs = matches ? qualifyingDocs.map(match => (match.metadata as Metadata).chunk) : [];
+  let docs = matches ? qualifyingDocs.map((match) => (match.metadata as Metadata).chunk) : [];
+  console.log("docs", docs.length);
   // Join all the chunks of text together, truncate to the maximum number of tokens, and return the result
   return docs.join("\n").substring(0, maxTokens)
 }
