cd45b2168af66a74b4c70fa26fdfced287ffd4f7
Rami Maalouf <rami.rami@ucalgary.ca>
2023-09-17 07:33:28 -0400

    removed sidebar

--- src/app/components/Chat/index.tsx
+++ src/app/components/Chat/index.tsx
@@ -18,21 +18,24 @@ const Chat: React.FC<Chat> = ({
   messages,
 }) => {
   return (
-    <div id="chat" className="flex flex-col w-full lg:w-3/5 mr-4 mx-5 lg:mx-0">
+    <div
+      id="chat"
+      className="flex flex-col w-full mx-5 lg:mx-0"
+    >
       <Messages messages={messages} />
       <>
         <form
           onSubmit={handleMessageSubmit}
-          className="mt-5 mb-5 relative bg-gray-700 rounded-lg"
+          className="relative mt-5 mb-5 bg-gray-700 rounded-lg"
         >
           <input
             type="text"
-            className="input-glow appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline pl-3 pr-10 bg-gray-600 border-gray-600 transition-shadow duration-200"
+            className="w-full px-3 py-2 pl-3 pr-10 leading-tight text-gray-200 transition-shadow duration-200 bg-gray-600 border border-gray-600 rounded appearance-none input-glow focus:outline-none focus:shadow-outline"
             value={input}
             onChange={handleInputChange}
           />
 
-          <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none text-gray-400">
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 pointer-events-none">
             Press ⮐ to send
           </span>
         </form>
