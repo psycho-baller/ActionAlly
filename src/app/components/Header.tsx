3eb163b3a2fda085dc55ccc2081ad5c6b2cc4eca
Rami Maalouf <rami.rami@ucalgary.ca>
2023-09-17 07:33:16 -0400

    redesigned the website header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,26 +1,15 @@
 import Image from "next/image";
-import PineconeLogo from "../../../public/pinecone.svg";
-import VercelLogo from "../../../public/vercel.svg";
+import Logo from "../../../public/logo.png";
 
 export default function Header({ className }: { className?: string }) {
   return (
-    <header
-      className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}
-    >
+    <header className={`${className}`}>
       <Image
-        src={PineconeLogo}
-        alt="pinecone-logo"
-        width="230"
+        src={Logo}
+        alt="logo"
+        width="100"
         height="50"
         className="ml-3"
-      />{" "}
-      <div className="text-4xl ml-3 mr-3">+</div>
-      <Image
-        src={VercelLogo}
-        alt="vercel-logo"
-        width="160"
-        height="50"
-        className="mr-3 mt-3"
       />
     </header>
   );
